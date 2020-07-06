cae2efcb3697aee0cf4cb386036cfe22b4247a0b
Ali Al Shaikhly <alshaikhly89@gmail.com>
2020-07-06 17:30:26 -0600

    Adding Buttons and DayList items

---
+++ src/components/DayListItem.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classnames from "classnames";
+
+
+export default function DayListItem(props) {
+
+  const formatSpots = (spots) => {
+    let finalFormat;
+    
+    switch (spots) {
+      case 0:
+        finalFormat = 'no spots remaining';
+        break;
+      case 1:
+        finalFormat = '1 spot remaining';
+        break;
+      default:
+        finalFormat = `${spots} spots remaining`;
+        break;
+    }
+    return finalFormat;
+  }
+  
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots===0
+  });
+
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+  <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
