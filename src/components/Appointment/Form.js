ecd6954d397145db036919bd7aa28cd5ba2547b7
Ali Al Shaikhly <alshaikhly89@gmail.com>
2020-07-13 17:07:07 -0600

    Addd Appointment tests for the form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,7 +5,8 @@ import Button from "components/Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  
+  const [error, setError] = useState("");
+
   const reset = () => {
     setInterviewer(null);
     setName('');
@@ -14,6 +15,15 @@ export default function Form(props) {
   const cancel = () => {
     props.onCancel()
     reset()
+  }
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
   }
   
   return (
@@ -27,17 +37,19 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             onChange={(event) => setName(event.target.value)}
             onSubmit={event => event.preventDefault()}
+            data-testid="student-name-input"
             /*
               This must be a controlled component
             */
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
+          <Button onClick={() => validate()} confirm>Save</Button>
         </section>
       </section>
     </main>
